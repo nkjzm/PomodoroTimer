76e9a813dda30c7ed874868f4b71454f24b75405
Nakaji Kohki <boomerang.cube@gmail.com>
2021-02-02 16:21:53 +0900

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by 中地功貴 on 2021/02/02.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
